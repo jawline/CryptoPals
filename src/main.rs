389e1fc4a3e15a0b8578082179359d518ee0cf86
Blake Loring <blake_l@parsed.uk>
2016-04-21 19:04:45 +0100

    Allow for multiple challenges in same binary

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,8 @@ use rustc_serialize::hex::FromHex;
 use rustc_serialize::hex::ToHex;
 
 fn main() {
-    println!("{}", xor::xor(std::env::args().nth(1).unwrap().from_hex().unwrap(), std::env::args().nth(2).unwrap().from_hex().unwrap()).to_hex());
+	match std::env::args().nth(0) {
+    	"xor" => { println!("{}", xor::xor(std::env::args().nth(2).unwrap().from_hex().unwrap(), std::env::args().nth(3).unwrap().from_hex().unwrap()).to_hex()); }
+		_ => { println!("No selection"); }
+	}
 }
