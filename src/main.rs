c5bf8de2068dbae6fba643ffeaf50ca271592f84
Blake Loring <blake_l@parsed.uk>
2016-04-21 19:09:49 +0100

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,15 @@ use rustc_serialize::hex::FromHex;
 use rustc_serialize::hex::ToHex;
 
 fn main() {
-	match std::env::args().nth(0) {
-    	"xor" => { println!("{}", xor::xor(std::env::args().nth(2).unwrap().from_hex().unwrap(), std::env::args().nth(3).unwrap().from_hex().unwrap()).to_hex()); }
-		_ => { println!("No selection"); }
+	match std::env::args().nth(1) {
+    	Some(ref x) if x == "xor" => {
+    		let arg_one = std::env::args().nth(2).unwrap().from_hex().unwrap();
+    		let arg_two = std::env::args().nth(3).unwrap().from_hex().unwrap();
+    		println!("{}", xor::xor(arg_one, arg_two).to_hex());
+    	},
+		Some(ref x) => {
+			println!("No selection {}", x);
+		}
+		_ => { println!("Incorrect number of arguments"); }
 	}
 }
