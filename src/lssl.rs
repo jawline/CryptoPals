859bae86220e7be4d9cbe02c3220b0af9e90d907
Blake Loring <blake_l@parsed.uk>
2016-04-25 13:23:43 +0100

    Add cbc shell to lssl

--- src/lssl.rs
+++ src/lssl.rs
@@ -1,17 +1,26 @@
-use openssl::crypto::symm::decrypt;
+use openssl::crypto::symm::{encrypt, decrypt};
 use std::string::{String, FromUtf8Error};
 use openssl::crypto::symm::Type::AES_128_ECB;
 use string_score::*;
 use hamming;
 use std::cmp::Ordering;
 
-pub fn decrypt_message(data: &Vec<u8>, key: &Vec<u8>) -> Result<String, FromUtf8Error> {
+pub fn decrypt_message(data: &Vec<u8>, key: &Vec<u8>) -> Vec<u8> {
 	let iv = vec![0; 128];
-	String::from_utf8(decrypt(AES_128_ECB, key, &iv, data))
+	decrypt(AES_128_ECB, key, &iv, data)
+}
+
+pub fn encrypt_message(data: &Vec<u8>, key: &Vec<u8>) -> Vec<u8> {
+	let iv = vec![0; 128];
+	encrypt(AES_128_ECB, key, &iv, data)
+}
+
+pub fn cbc_decrypt(data: &Vec<u8>, key: &Vec<u8>) -> Vec<u8> {
+	encrypt_message(data, key)
 }
 
 pub fn find_english(data: &Vec<Vec<u8>>, key: &Vec<u8>) -> String {
-	let mut possibles: Vec<String> = data.iter().map(|item| decrypt_message(item, key)).filter(|x| x.is_ok()).map(|x| x.unwrap()).collect();
+	let mut possibles: Vec<String> = data.iter().map(|item| String::from_utf8(decrypt_message(item, key))).filter(|x| x.is_ok()).map(|x| x.unwrap()).collect();
 	possibles.sort_by(|x, y| score_string(x).partial_cmp(&score_string(y)).unwrap_or(Ordering::Equal));
 	possibles.pop().unwrap()
 }
