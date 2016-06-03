56c370468fa5298e0a5c95d0f4ae59a1c94ec167
Blake Loring <blake_l@parsed.uk>
2016-06-03 11:03:23 +0100

    Remove warning in generate_key

--- src/random.rs
+++ src/random.rs
@@ -3,5 +3,5 @@ use rand::Rng;
 
 pub fn generate_key(length: usize) -> Vec<u8> {
 	let mut rng = OsRng::new().unwrap();
-	(0..length).map(|x| rng.gen::<u8>()).collect()
+	(0..length).map(|_| rng.gen::<u8>()).collect()
 }
